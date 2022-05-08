b6cb2ee6b9dbe68917866cb3980905210396c4a5
Ankit <ankitthakur1998@gmail.com>
2022-05-08 23:17:57 +0530

    add mapping for POST /employees - add new employee

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,6 +46,28 @@ public class EmployeeRestController {
 		return theEmployee;
 	}
 	
+	// add mapping for POST /employees - add new employee
 	
-	
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+		
+		// also just in case they pass an id in JSON ... set id to 0
+		// this is to force a save of new item ... instead of update
+		
+		theEmployee.setId(0);
+		
+		employeeService.save(theEmployee);
+		
+		return theEmployee;
+		
+	}
 }
+
+
+
+
+
+
+
+
+
