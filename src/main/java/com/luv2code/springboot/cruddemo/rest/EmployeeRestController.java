595bf42136366255f4c1313173a8ad1ea5a729ae
Ankit <ankitthakur1998@gmail.com>
2022-05-09 01:25:12 +0530

    New project 22-jpa-cruddemo created

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -75,8 +75,20 @@ public class EmployeeRestController {
 	
 	// add mapping for DELETE /employees/{employeeId} - delete employee
 	
-	
+	@DeleteMapping("/employees/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId) {
 		
+		Employee tempEmployee = employeeService.findById(employeeId);
+		
+		// throw exception if null
+		
+		if(tempEmployee == null) {
+			throw new RuntimeException("Employee id not found - " + employeeId);
+		}
+		
+		employeeService.deleteById(employeeId);
+		
+		return "Deleted employee id - " + employeeId;
 	}
 	
 }
