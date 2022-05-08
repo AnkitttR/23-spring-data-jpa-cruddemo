b24d484ac3faf4caf0877e0d5cf5bd4ee1c2adb9
Ankit <ankitthakur1998@gmail.com>
2022-05-08 22:20:15 +0530

    / constructor injection

--- src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -2,9 +2,19 @@ package com.luv2code.springboot.cruddemo.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
+	
+	private EmployeeDAO employeeDAO;
+	
+	public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {  // constructor injection
+		employeeDAO = theEmployeeDAO;
+	}
 
 	@Override
 	public List<Employee> findAll() {
