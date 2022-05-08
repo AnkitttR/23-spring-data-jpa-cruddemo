794cab8208abedaf25a53fd82164fbf0855889d8
Ankit <ankitthakur1998@gmail.com>
2022-05-09 01:39:57 +0530

    Comments added

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
@@ -13,30 +14,41 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO {
 
 	private EntityManager entityManager;
 	
-	
+	@Autowired
+	public EmployeeDAOJpaImpl(EntityManager thEntityManager) {  // Constructor Injection
+		entityManager = thEntityManager;
+	}
 	
 	
 	@Override
 	public List<Employee> findAll() {
-		// TODO Auto-generated method stub
+		
+		// create a query
+		
+		
+		// execute query and get result list
+		
+		
+		// return the results
+		
 		return null;
 	}
 
 	@Override
 	public Employee findById(int theId) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public void save(Employee theEmployee) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
 	@Override
 	public void deleteById(int theId) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
