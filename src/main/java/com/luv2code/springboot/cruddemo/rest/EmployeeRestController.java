da72b8354c4ecebf43a33f4e72514e0048bcc58b
Ankit <ankitthakur1998@gmail.com>
2022-05-08 21:28:16 +0530

    Using constructor injection EmployeeRestController class

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -1,9 +1,22 @@
 package com.luv2code.springboot.cruddemo.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
+
+@RestController
+@RequestMapping("/api")
 public class EmployeeRestController {
 
-	// quick & dirty: inject employee DAO
+	private EmployeeDAO employeeDAO;
 	
+	// quick & dirty: inject employee DAO (Use constructor injection)
+	@Autowired
+	public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
+		employeeDAO = theEmployeeDAO;
+	}
 	
 	// expose "/employees" & return list of employees
 }
