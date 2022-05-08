d46701ad01c959c47fade4ea4656707f626ee88d
Ankit <ankitthakur1998@gmail.com>
2022-05-09 01:31:42 +0530

    New class EmployeeDAOJpaImpl created

---
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -0,0 +1,33 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import java.util.List;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+
+public class EmployeeDAOJpaImpl implements EmployeeDAO {
+
+	@Override
+	public List<Employee> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee findById(int theId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
