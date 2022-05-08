95f9006d0fdbaeee6fded99b7fe74e673c78baf8
Ankit <ankitthakur1998@gmail.com>
2022-05-08 20:53:21 +0530

    comments added

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -1,5 +1,24 @@
 package com.luv2code.springboot.cruddemo.dao;
 
-public class EmployeeDAOHibernateImpl {
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+
+@Repository
+public class EmployeeDAOHibernateImpl implements EmployeeDAO {
+
+	// define field for entitymanager
+	
+	
+	// setup constructor injection
+	
+	
+	@Override
+	public List<Employee> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
