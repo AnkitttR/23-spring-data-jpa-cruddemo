0bc474621d96073ae60ed199336a6fd4f74215fd
Ankit <ankitthakur1998@gmail.com>
2022-05-08 20:46:07 +0530

    Employee entity is imported to EmployeeDAO for getting list of all employees

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAO.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAO.java
@@ -1,5 +1,10 @@
 package com.luv2code.springboot.cruddemo.dao;
 
+import java.util.List;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+
 public interface EmployeeDAO {
 
+	public List<Employee> findAll();
 }
