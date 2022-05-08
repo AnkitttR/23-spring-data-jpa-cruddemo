7f429ab745e9e7960d4b49226b1b5d40a0386fed
Ankit <ankitthakur1998@gmail.com>
2022-05-08 20:37:13 +0530

    Getter & Setter methods only defined here, but not executed here

--- src/main/java/com/luv2code/springboot/cruddemo/entity/Employee.java
+++ src/main/java/com/luv2code/springboot/cruddemo/entity/Employee.java
@@ -37,8 +37,40 @@ public class Employee {
 		this.email = email;
 	}
 	
-	
 	// define getter/setter
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 	
 	// define toString 
 }
