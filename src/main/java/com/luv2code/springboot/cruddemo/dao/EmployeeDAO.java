e42b04a5535d9f3dd0f88a66a5e2b3f3ec901157
Ankit <ankitthakur1998@gmail.com>
2022-05-08 21:41:27 +0530

    Three new methods defined in EmployeeDAO interface

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAO.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAO.java
@@ -7,4 +7,11 @@ import com.luv2code.springboot.cruddemo.entity.Employee;
 public interface EmployeeDAO {
 
 	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+	
 }
