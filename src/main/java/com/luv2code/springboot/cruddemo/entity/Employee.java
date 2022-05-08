22e011360ce16674ad1f22e909f94f1549afeee0
Ankit <ankitthakur1998@gmail.com>
2022-05-08 20:27:59 +0530

    Column name should be like this

--- src/main/java/com/luv2code/springboot/cruddemo/entity/Employee.java
+++ src/main/java/com/luv2code/springboot/cruddemo/entity/Employee.java
@@ -17,10 +17,10 @@ public class Employee {
 	@Column(name="id")
 	private int id;
 	
-	@Column(name="firstName")
+	@Column(name="first_name")
 	private String firstName;
 	
-	@Column(name="lastName")
+	@Column(name="last_name")
 	private String lastName;
 	
 	@Column(name="email")
