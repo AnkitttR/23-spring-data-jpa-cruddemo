48294b2c686f65b36d1607e702d3f3916328da50
Ankit <ankitthakur1998@gmail.com>
2022-05-08 21:07:52 +0530

    Get the current hibernate session

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 
@@ -23,9 +25,11 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO {
 	
 	
 	@Override
+	@Transactional
 	public List<Employee> findAll() {
 		
 		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
 		
 		// create a query
 		
