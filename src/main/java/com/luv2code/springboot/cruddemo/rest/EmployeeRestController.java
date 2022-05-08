eba670832e34a8a10dd438f52051215c549fdae6
Ankit <ankitthakur1998@gmail.com>
2022-05-08 21:22:01 +0530

    comments added to EmployeeRestController

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -2,4 +2,8 @@ package com.luv2code.springboot.cruddemo.rest;
 
 public class EmployeeRestController {
 
+	// quick & dirty: inject employee DAO
+	
+	
+	// expose "/employees" & return list of employees
 }
