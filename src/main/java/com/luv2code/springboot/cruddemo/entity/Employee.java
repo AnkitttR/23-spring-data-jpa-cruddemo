b6dfc6a20ad72cafc6e6e490666c4b54783f52eb
Ankit <ankitthakur1998@gmail.com>
2022-05-08 20:19:39 +0530

    Comments added

--- src/main/java/com/luv2code/springboot/cruddemo/entity/Employee.java
+++ src/main/java/com/luv2code/springboot/cruddemo/entity/Employee.java
@@ -1,5 +1,17 @@
 package com.luv2code.springboot.cruddemo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
 public class Employee {
 
+	// define fields
+	
+	// define constructors
+	
+	// define getter/setter
+	
+	// define toString 
 }
