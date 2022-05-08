f596844f2e38b97c12e6e31bac8d431c6adad9b9
Ankit <ankitthakur1998@gmail.com>
2022-05-09 01:35:07 +0530

    Private field EntityManager declared

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -2,10 +2,20 @@ package com.luv2code.springboot.cruddemo.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import org.springframework.stereotype.Repository;
+
 import com.luv2code.springboot.cruddemo.entity.Employee;
 
+@Repository
 public class EmployeeDAOJpaImpl implements EmployeeDAO {
 
+	private EntityManager entityManager;
+	
+	
+	
+	
 	@Override
 	public List<Employee> findAll() {
 		// TODO Auto-generated method stub
