7236d89cbae6c055130513e8a2c0364fc219c1ef
Ankit <ankitthakur1998@gmail.com>
2022-05-09 02:58:33 +0530

    EmployeeRepository Interface created

---
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<T, ID> {
+
+}
