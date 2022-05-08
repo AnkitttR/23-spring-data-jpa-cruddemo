7e40d022a1addebca96f40f9c526dd85ae7dc263
Ankit <ankitthakur1998@gmail.com>
2022-05-08 22:15:21 +0530

    New class EmployeeServiceImpl created

---
+++ src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,33 @@
+package com.luv2code.springboot.cruddemo.service;
+
+import java.util.List;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Override
+	public List<Employee> findAll() {
+		
+		return null;
+	}
+
+	@Override
+	public Employee findById(int theId) {
+		
+		return null;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		
+
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		
+
+	}
+
+}
