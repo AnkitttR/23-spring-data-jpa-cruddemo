111b26b560f8f20ff1834afa9d92ab12e3bba8dc
Ankit <ankitthakur1998@gmail.com>
2022-05-08 21:36:52 +0530

    list method is successfully running on localhost

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -1,10 +1,14 @@
 package com.luv2code.springboot.cruddemo.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
+import com.luv2code.springboot.cruddemo.entity.Employee;
 
 @RestController
 @RequestMapping("/api")
@@ -19,4 +23,10 @@ public class EmployeeRestController {
 	}
 	
 	// expose "/employees" & return list of employees
+	@GetMapping("/employees")
+	public List<Employee> findAll() {
+		
+		return employeeDAO.findAll();
+		
+	}
 }
