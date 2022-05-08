fbb91c86a186eea9d95f264c0c19eef0d123665a
Ankit <ankitthakur1998@gmail.com>
2022-05-09 03:01:15 +0530

    EmployeeRepository Interface completed

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,9 @@ package com.luv2code.springboot.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<T, ID> {
+import com.luv2code.springboot.cruddemo.entity.Employee;
 
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// that's it ... no need to write any code LOL!
 }
