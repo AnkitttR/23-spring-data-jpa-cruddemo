b820577b3afda134154b5f9e11aa2cacd130c561
Ankit <ankitthakur1998@gmail.com>
2022-05-09 01:56:14 +0530

    EmployeeServiceImpl is given beanId employeeDAOJpaImpl with the help of @Qualifier

--- src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.cruddemo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,7 +16,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 	private EmployeeDAO employeeDAO;
 	
 	@Autowired
-	public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {  // constructor injection
+	public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO theEmployeeDAO) {  // constructor injection
 		employeeDAO = theEmployeeDAO;
 	}
 
